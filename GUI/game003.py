dde1f324083e897ca492bc5d3bf575477cc4b8da
pinhulk <pinhugtot@hotmail.com>
2023-04-13 18:47:49 +0700

    Update game003.py
    done frontend but no rotation the obstacle

--- GUI/game003.py
+++ GUI/game003.py
@@ -157,6 +157,7 @@ class howtoplay(Screen):
 
 class L1_static(Screen):       
     def on_enter(self):
+        print('l1 static game start')
         l1_layout = FloatLayout()
         with self.canvas:
             Rectangle(size = (488,420),pos = (150,120)) # size /10 *4
@@ -167,6 +168,8 @@ class L1_static(Screen):
             Ellipse(size=(32,32),pos=(186,252))
             Ellipse(size=(32,32),pos=(582,392))
 
+        l1Label = Label(text = 'LEVEL1', font_size = 30, pos_hint = {'x':0,'y':0.48})
+
         moriaLabel = Label(text = 'Moria',pos_hint={'x':-0.45,'y':0.48})
         self.moriaStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.47})
         self.moriaStamina.value = 100
@@ -174,13 +177,36 @@ class L1_static(Screen):
         pauseBut = Button(text ='| |',font_size = 40,size_hint=(None, None), size=(50, 50),pos_hint={'x':0.82,'y':0.85})
         pauseBut.bind(on_release = self.pause_callback)
 
+        self.goal_var = 0
+        print(self.goal_var)
+        testBut = Button(text = 'goal', size_hint=(None,None), size=(50,30),pos_hint={'x':0.93,'y':0})
+        testBut.bind(on_release = self.reachgoal)
+        l1_layout.add_widget(testBut)
+
         l1_layout.add_widget(self.moriaStamina)
+        l1_layout.add_widget(l1Label)
         l1_layout.add_widget(moriaLabel)
         l1_layout.add_widget(pauseBut)
 
         self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
 
+
         self.add_widget(l1_layout)
+
+    def reachgoal(self,instance):
+        print(self.goal_var)
+        self.goal_var = 1
+        if self.goal_var == 1:
+            self.clear_widgets()
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.canvas.clear()
+            Text = "[size=60]LEVEL 2[/size]\n[size=11](static)[/size]"
+            self.connect = Label(text=Text, markup=True)
+            self.add_widget(self.connect)
+            self.goal_var = 0
+            Clock.schedule_once(self.static_game, 3)  # Change screen after 5 seconds
 
     def lowerStamina(self,dt):
         self.moriaStamina.value = self.moriaStamina.value - 30
@@ -191,6 +217,11 @@ class L1_static(Screen):
                 Clock.unschedule(self.clock_event)
                 self.clock_event = None
             self.manager.current = 'vicpop'
+
+
+    def static_game(self,dt):
+        print("L1 ending")
+        self.manager.current = 'L2S'
 
     def pause_callback(self, *args):
         print('pause')
@@ -287,6 +318,9 @@ class L1_dynamic(Screen):
             Color(1,0.8,0.45,1)
             Ellipse(size=(32,32),pos=(186,252))
             Ellipse(size=(32,32),pos=(582,392))
+
+        l1Label = Label(text = 'LEVEL1', font_size = 30, pos_hint = {'x':0,'y':0.48})
+
         self.moriaStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.47})
         self.playerStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.43})
         playerLabel = Label(text = 'You',pos_hint={'x':-0.45,'y':0.44})
@@ -302,6 +336,12 @@ class L1_dynamic(Screen):
         pauseBut = Button(text ='| |',font_size = 40,size_hint=(None, None), size=(50, 50),pos_hint={'x':0.82,'y':0.85})
         pauseBut.bind(on_release = self.pause_callback)
 
+        self.goal_var = 0
+        print(self.goal_var)
+        testBut = Button(text = 'goal', size_hint=(None,None), size=(50,30),pos_hint={'x':0.93,'y':0})
+        testBut.bind(on_release = self.reachgoal)
+        l1_layout.add_widget(testBut)
+
         l1_layout.add_widget(self.moriaStamina)
         l1_layout.add_widget(self.playerStamina)
         l1_layout.add_widget(playerLabel)
@@ -309,6 +349,7 @@ class L1_dynamic(Screen):
         l1_layout.add_widget(pauseBut)
         l1_layout.add_widget(leftaimming)
         l1_layout.add_widget(rightaimming)
+        l1_layout.add_widget(l1Label)
 
         self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
 
@@ -323,6 +364,26 @@ class L1_dynamic(Screen):
                 Clock.unschedule(self.clock_event)
                 self.clock_event = None
             self.manager.current = 'vicpop'
+
+    def reachgoal(self,instance):
+        print(self.goal_var)
+        self.goal_var = 1
+        if self.goal_var == 1:
+            self.clear_widgets()
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.canvas.clear()
+            Text = "[size=60]LEVEL 2[/size]\n[size=11](dynamic)[/size]"
+            self.connect = Label(text=Text, markup=True)
+            self.add_widget(self.connect)
+            self.goal_var = 0
+            Clock.schedule_once(self.dynamic_game, 3)  # Change screen after 5 seconds
+
+    def dynamic_game(self,dt):
+        print("L1 ending")
+        self.manager.current = 'L2D'
+
 
     def pause_callback(self, *args):
         print('pause')
@@ -407,10 +468,298 @@ class L1_dynamic(Screen):
                 # Code to be executed when the button is disabled
                 print("right disabled!")
 
+
+class L2_static(Screen):
+    def on_enter(self):
+        l2_layout = FloatLayout()
+        with self.canvas:
+            Rectangle(size = (488,420),pos = (150,120)) # size /10 *4
+            Color(0,1,0,0.8)
+            Rectangle(size = (92,8),pos = (204,300))
+            Rectangle(size = (92,8),pos = (348,300))
+            Rectangle(size = (92,8),pos = (492,300))
+            Rectangle(size = (92,8),pos = (276,412))
+            Rectangle(size = (92,8),pos = (420,412))
+            Color(1,0.8,0.45,1)
+            Ellipse(size=(32,32),pos=(234,288))
+            Ellipse(size=(32,32),pos=(378,288))
+            Ellipse(size=(32,32),pos=(522,288))
+            Ellipse(size=(32,32),pos=(306,400))
+            Ellipse(size=(32,32),pos=(450,400))
+            # Ellipse(size=(32,32),pos=(150,120))
+
+        l2Label = Label(text = 'LEVEL2', font_size = 30, pos_hint = {'x':0,'y':0.48})
+
+        moriaLabel = Label(text = 'Moria',pos_hint={'x':-0.45,'y':0.48})
+        self.moriaStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.47})
+        self.moriaStamina.value = 100
+
+        pauseBut = Button(text ='| |',font_size = 40,size_hint=(None, None), size=(50, 50),pos_hint={'x':0.82,'y':0.85})
+        pauseBut.bind(on_release = self.pause_callback)
+
+        self.goal_var = 0
+        print(self.goal_var)
+        testBut = Button(text = 'goal', size_hint=(None,None), size=(50,30),pos_hint={'x':0.93,'y':0})
+        testBut.bind(on_release = self.reachgoal)
+        l2_layout.add_widget(testBut)
+
+        l2_layout.add_widget(self.moriaStamina)
+        l2_layout.add_widget(moriaLabel)
+        l2_layout.add_widget(pauseBut)
+        l2_layout.add_widget(l2Label)
+
+        self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
+
+
+        self.add_widget(l2_layout)
+
+    def reachgoal(self,instance):
+        print(self.goal_var)
+        self.goal_var = 1
+        if self.goal_var == 1:
+            self.clear_widgets()
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.goal_var = 0
+        print("L2 ending --> player lose")
+        self.manager.current = 'loserpop'
+
+
+    def lowerStamina(self,dt):
+        self.moriaStamina.value = self.moriaStamina.value - 30
+        print('moria stamina = ', self.moriaStamina.value)
+        if self.moriaStamina.value <= 0:
+            print('YOU WIN!!')
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.manager.current = 'vicpop'
+
+
+    def pause_callback(self, *args):
+        print('pause')
+        if self.clock_event:
+            Clock.unschedule(self.clock_event)
+            self.clock_event = None
+
+        pausePopLayout = FloatLayout()
+        resumeBut = Button(text = 'RESUME',size_hint=(None, None),size=(300, 100), pos_hint = {'center_x':0.5,'top':0.85})
+        resumeBut.bind(on_release = self.resume_callback)
+        backHome = Button(text = 'HOME',size_hint=(None, None),size=(300, 100), pos_hint = {'center_x':0.5,'top':0.45})
+        backHome.bind(on_release = self.home_callback)
+        pausePopLayout.add_widget(resumeBut)
+        pausePopLayout.add_widget(backHome)
+        self.pausepopup = Popup(title='PAUSE',content= pausePopLayout, size_hint=(None, None), size=(400, 350))
+        self.pausepopup.open()
+    
+    def resume_callback(self, instance):
+        print('resume dai jaaa')
+        self.pausepopup.dismiss()
+        if not self.clock_event:
+            self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
+
+    def home_callback(self,instance):
+        print('go back home')
+        homeconfirmPopLayout = FloatLayout()
+        homeLabel = Label(text = 'HOME ?',font_size = 60,pos_hint = {'center_x':0.5,'y':0.25})
+        okBut = Button(text = 'OK',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.25,'top':0.45})
+        cancleBut = Button(text = 'CANCLE',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.75,'top':0.45})
+        okBut.bind(on_release = self.gotomain)
+        cancleBut.bind(on_release = self.cancleCB)
+        homeconfirmPopLayout.add_widget(homeLabel)
+        homeconfirmPopLayout.add_widget(okBut)
+        homeconfirmPopLayout.add_widget(cancleBut)
+        self.homepopup = Popup(title='',content= homeconfirmPopLayout, size_hint=(None, None), size=(400, 300))
+        self.homepopup.open()
+
+    def gotomain(self,instance):
+        print('go to main')
+        self.pausepopup.dismiss()
+        self.homepopup.dismiss()
+        self.manager.current = 'mainS'
+
+    def cancleCB(self,instance):
+        print('cancle')
+        self.homepopup.dismiss()
+
+
+class L2_dynamic(Screen):
+    def on_enter(self):
+        print('L2 dynamic game start')
+        l2_layout = FloatLayout()
+        with self.canvas:
+            Rectangle(size = (488,420),pos = (150,120)) # size /10 *4
+            Color(0,1,0,0.8)
+            Rectangle(size = (92,8),pos = (204,300))
+            Rectangle(size = (92,8),pos = (348,300))
+            Rectangle(size = (92,8),pos = (492,300))
+            Rectangle(size = (92,8),pos = (276,412))
+            Rectangle(size = (92,8),pos = (420,412))
+            Color(1,0.8,0.45,1)
+            Ellipse(size=(32,32),pos=(234,288))
+            Ellipse(size=(32,32),pos=(378,288))
+            Ellipse(size=(32,32),pos=(522,288))
+            Ellipse(size=(32,32),pos=(306,400))
+            Ellipse(size=(32,32),pos=(450,400))
+            # Ellipse(size=(32,32),pos=(150,120))
+
+        l2Label = Label(text = 'LEVEL2', font_size = 30, pos_hint = {'x':0,'y':0.48})
+
+        moriaLabel = Label(text = 'Moria',pos_hint={'x':-0.45,'y':0.48})
+        self.moriaStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.47})
+        self.moriaStamina.value = 100
+        self.playerStamina = ProgressBar(max=100,size_hint_x=0.2,pos_hint={'x':0.1,'y':0.43})
+        playerLabel = Label(text = 'You',pos_hint={'x':-0.45,'y':0.44})
+        self.playerStamina.value = 100
+
+        rotateBut = Button(text = 'Trick',size_hint=(None,None),size=(100,50),pos = (350,50))
+        rotateBut.bind(on_release = self.rotateCB)
+
+        pauseBut = Button(text ='| |',font_size = 40,size_hint=(None, None), size=(50, 50),pos_hint={'x':0.82,'y':0.85})
+        pauseBut.bind(on_release = self.pause_callback)
+
+        self.goal_var = 0
+        print(self.goal_var)
+        testBut = Button(text = 'goal', size_hint=(None,None), size=(50,30),pos_hint={'x':0.93,'y':0})
+        testBut.bind(on_release = self.reachgoal)
+        l2_layout.add_widget(testBut)
+
+        l2_layout.add_widget(self.moriaStamina)
+        l2_layout.add_widget(moriaLabel)
+        l2_layout.add_widget(self.playerStamina)
+        l2_layout.add_widget(playerLabel)
+        l2_layout.add_widget(pauseBut)
+        l2_layout.add_widget(l2Label)
+        l2_layout.add_widget(rotateBut)
+
+        self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
+
+
+        self.add_widget(l2_layout)
+
+    def reachgoal(self,instance):
+        print(self.goal_var)
+        self.goal_var = 1
+        if self.goal_var == 1:
+            self.clear_widgets()
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.goal_var = 0
+        print("L2 ending --> player lose")
+        self.manager.current = 'loserpop'
+
+
+    def lowerStamina(self,dt):
+        self.moriaStamina.value = self.moriaStamina.value - 30
+        print('moria stamina = ', self.moriaStamina.value)
+        if self.moriaStamina.value <= 0:
+            print('YOU WIN!!')
+            if self.clock_event:
+                Clock.unschedule(self.clock_event)
+                self.clock_event = None
+            self.manager.current = 'vicpop'
+
+
+    def pause_callback(self, *args):
+        print('pause')
+        if self.clock_event:
+            Clock.unschedule(self.clock_event)
+            self.clock_event = None
+
+        pausePopLayout = FloatLayout()
+        resumeBut = Button(text = 'RESUME',size_hint=(None, None),size=(300, 100), pos_hint = {'center_x':0.5,'top':0.85})
+        resumeBut.bind(on_release = self.resume_callback)
+        backHome = Button(text = 'HOME',size_hint=(None, None),size=(300, 100), pos_hint = {'center_x':0.5,'top':0.45})
+        backHome.bind(on_release = self.home_callback)
+        pausePopLayout.add_widget(resumeBut)
+        pausePopLayout.add_widget(backHome)
+        self.pausepopup = Popup(title='PAUSE',content= pausePopLayout, size_hint=(None, None), size=(400, 350))
+        self.pausepopup.open()
+
+    def rotateCB(self,instance):
+        print('rotate')
+        print('player stamina = ', self.playerStamina.value)
+
+        if self.playerStamina.value <= 0:
+            print("player's stamina is out!!")
+        else:
+            self.playerStamina.value = self.playerStamina.value - 10
+
+    def resume_callback(self, instance):
+        print('resume dai jaaa')
+        self.pausepopup.dismiss()
+        if not self.clock_event:
+            self.clock_event = Clock.schedule_interval(self.lowerStamina, 1.5)
+
+    def home_callback(self,instance):
+        print('go back home')
+        homeconfirmPopLayout = FloatLayout()
+        homeLabel = Label(text = 'HOME ?',font_size = 60,pos_hint = {'center_x':0.5,'y':0.25})
+        okBut = Button(text = 'OK',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.25,'top':0.45})
+        cancleBut = Button(text = 'CANCLE',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.75,'top':0.45})
+        okBut.bind(on_release = self.gotomain)
+        cancleBut.bind(on_release = self.cancleCB)
+        homeconfirmPopLayout.add_widget(homeLabel)
+        homeconfirmPopLayout.add_widget(okBut)
+        homeconfirmPopLayout.add_widget(cancleBut)
+        self.homepopup = Popup(title='',content= homeconfirmPopLayout, size_hint=(None, None), size=(400, 300))
+        self.homepopup.open()
+
+    def gotomain(self,instance):
+        print('go to main')
+        self.pausepopup.dismiss()
+        self.homepopup.dismiss()
+        self.manager.current = 'mainS'
+
+    def cancleCB(self,instance):
+        print('cancle')
+        self.homepopup.dismiss()
+
+class loserPopup(Screen):
+    def on_enter(self):
+        print("LOSER")
+        loserPopLayout = FloatLayout()
+        loserLabel = Label(text = 'LOSER !!',font_size = 60,pos_hint = {'center_x':0.5,'y':0.3})
+        loserLabel2 = Label(text = "You are FIRED !!.",font_size = 22,pos_hint = {'center_x':0.5,'y':0.1})
+        backHome = Button(text = 'HOME',size_hint=(None, None),size=(300, 100), pos_hint = {'center_x':0.5,'top':0.45})
+        backHome.bind(on_release = self.home_callback)
+        loserPopLayout.add_widget(loserLabel)
+        loserPopLayout.add_widget(loserLabel2)
+        loserPopLayout.add_widget(backHome)
+        self.vicpopup = Popup(title='',content= loserPopLayout, size_hint=(None, None), size=(400, 300))
+        self.vicpopup.open()
+
+    def home_callback(self,instance):
+        print('go back home')
+        homeconfirmPopLayout = FloatLayout()
+        homeLabel = Label(text = 'HOME ?',font_size = 60,pos_hint = {'center_x':0.5,'y':0.25})
+        okBut = Button(text = 'OK',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.25,'top':0.45})
+        cancleBut = Button(text = 'CANCLE',size_hint=(None, None),size=(150, 100), pos_hint = {'center_x':0.75,'top':0.45})
+        okBut.bind(on_release = self.gotomain)
+        cancleBut.bind(on_release = self.cancleCB)
+        homeconfirmPopLayout.add_widget(homeLabel)
+        homeconfirmPopLayout.add_widget(okBut)
+        homeconfirmPopLayout.add_widget(cancleBut)
+        self.homepopup = Popup(title='',content= homeconfirmPopLayout, size_hint=(None, None), size=(400, 300))
+        self.homepopup.open()
+
+    def gotomain(self,instance):
+        print('go to main')
+        self.vicpopup.dismiss()
+        self.homepopup.dismiss()
+        self.manager.current = 'mainS'
+
+    def cancleCB(self,instance):
+        print('cancle')
+        self.homepopup.dismiss()
+
 class tryScreen(Screen):
     def on_enter(self):
-        user_input = input("Goal?: ")
-        print(user_input)
+        pass
+
+
 
         
 class HMmission_stoptheRescueRobot(App):
@@ -425,7 +774,10 @@ class HMmission_stoptheRescueRobot(App):
         self.sm.add_widget(howtoplay(name='htp'))
         self.sm.add_widget(L1_static(name='L1S'))
         self.sm.add_widget(L1_dynamic(name='L1D'))
+        self.sm.add_widget(L2_static(name='L2S'))
+        self.sm.add_widget(L2_dynamic(name='L2D'))
         self.sm.add_widget(vicpop(name='vicpop'))
+        self.sm.add_widget(loserPopup(name='loserpop'))
 
 
         self.sm.add_widget(tryScreen(name='ts'))
